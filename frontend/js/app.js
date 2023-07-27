114e2523ce1ebffe627d82cd729fa17b955ce57f
DylanBru <dylan.bru@gmail.com>
2023-07-27 16:37:03 +0200

    Ajouter une tâche via le formulaire d'ajout puis l'afficher dans la liste des tâches une fois l'ajout réussi

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,52 +1,10 @@
-// V1 pas opti
-// function getListTasks() {
-    // fetch("http://127.0.0.1:8000/api/tasks")     // Etape 1 : faire la requête
-        // .then(response => { // Etape 2 : Réception de la résponse HTTP
-            // Extraction du résultat de la réponse
-            // Produit une nouvelle promesse que l'on retourne pour chaîner les .then
-            // return response.json();
-        // }) 
-        // .then(listTasks => {   // Etape 3 : Réception du résultat
-
-            // vidange du container
-            // let container = document.querySelector("ul");
-            // container.textContent = '';
-        
-            // for (const task of listTasks) {
-                // création des éléments
-                // const liToAdd = document.createElement('li');
-                // liToAdd.setAttribute("id", task['id']);
-                // const pToAdd = document.createElement('p');
-                // pToAdd.textContent = task['title'];
-                // const deleteToAdd = document.createElement('div');
-                // deleteToAdd.classList.add("delete");
-                // const editToAdd = document.createElement('div');
-                // editToAdd.classList.add("edit");
-            
-                // Imbrication des éléments
-    //             liToAdd.append(pToAdd);
-    //             liToAdd.append(deleteToAdd);
-    //             liToAdd.append(editToAdd);
-    //             container.append(liToAdd);
-    //         }
-    //     })
-    // }
-    
-
-// getListTasks();
-
-
-
-
-
-
-// V2 beaucoup plus opti
-
 // Les const utiles 
+
+const container = document.querySelector("ul");
 
 const getListTasks = function(){
     return fetch("http://127.0.0.1:8000/api/tasks")     
-    .then(response => { 
+    .then(response => {
         return response.json();
     })
 }
@@ -55,15 +13,24 @@ const deleteTask = function(taskToDeleteId){
     return fetch('http://127.0.0.1:8000/api/tasks/' + taskToDeleteId, {method: 'DELETE'})
 }
 
-const container = document.querySelector
-("ul");
-
+const postNewTask = async function(newTaskTitle){
+    const request = await fetch('http://localhost:8000/api/tasks', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+           title: newTaskTitle 
+        })});
+    const tasks = await request.json();
+    return tasks;
+}
 
 const deleteContainer = function(){
     container.textContent = '';
 }
 
-
+// Création de l'élément task à afficher dans la liste des task
 const createTaskElement = (task) => {
     const liToAdd = document.createElement('li');
     liToAdd.setAttribute("id", task['id']);
@@ -96,7 +63,6 @@ const createTaskElement = (task) => {
             console.log("erreur lors de la suppression de la tâche")
         })
     })
-
     const editToAdd = document.createElement('div');
     editToAdd.classList.add("edit");
     liToAdd.append(editToAdd);
@@ -104,19 +70,81 @@ const createTaskElement = (task) => {
     container.append(liToAdd);
 }
 
+// Afficher le formulaire d'ajout d'une tâche
+const showFormAddTask = function(){
+    const newTaskButton = document.querySelector("#createTask");
 
+    newTaskButton.addEventListener('click', () => 
+    {
+        document.querySelector("header").classList.add("muted");
+        document.querySelector(".create-task-container").classList.add("d-none");
+        document.querySelector(".tasklist").classList.add("d-none");
+        document.querySelector(".modal-dialog").classList.add("show");
+    },
+)}
 
-// Afficher la list des tâches
-window.addEventListener('DOMContentLoaded', function(){
+// Désaficher le formulaire d'ajout d'une tâche
+const unShowFormAddTask = function ()
+{
+    document.querySelector("header").classList.remove("muted");
+    document.querySelector(".create-task-container").classList.remove("d-none");
+    document.querySelector(".tasklist").classList.remove("d-none");
+    document.querySelector(".modal-dialog").classList.remove("show");
+}
+
+// Gestion de l'affichage de la liste des tâches
+const showListTasks = function()
+    {
     getListTasks()
-    .then(listTasks => {
+    .then(listTasks => 
+        {
         deleteContainer();
         for (const task of listTasks) {
-            console.log(task)
+            // console.log(task)
             createTaskElement(task);
-    }})
-    .catch(() => {
+        }})
+    .catch(() => 
+    {
         console.log('Erreur de requête lors de l\'affichage des tâches');
     });
+    }
+
+
+window.addEventListener('DOMContentLoaded', function (){
+    // Gestion de l'affichage de la liste des tâches
+    showListTasks();
+    // Gestion de l'affichage du formulaire d'ajout de tâche
+    showFormAddTask();
 })
 
+// Gestion d'ajout d'une tâche
+document.querySelector("#submit").addEventListener('click', () => 
+    {
+        document.querySelector('form').addEventListener('submit', event => 
+        {
+            event.preventDefault();
+        });
+
+        newTaskTitle = document.querySelector("#task-title").value;
+        // console.log(newTaskTitle);
+        postNewTask(newTaskTitle)
+        .then (newTask => 
+            {
+                unShowFormAddTask();
+                showListTasks();
+                document.querySelector(".success").removeAttribute("hidden");
+
+                console.info(newTask);
+                
+                // Gestion de l'affichage du formulaire d'ajout de tâche
+                // showFormAddTask();
+                // location.reload();
+                // createTaskElement(newTask)
+                // document.querySelector(".success").removeAttribute("hidden");
+                // Désactiver l'affichafe du formulaire pour retourner sur la liste des tâches
+            })
+        .catch(() => 
+        {
+            console.log('Erreur de requête lors de l\'ajout de la tâche');
+        });
+    })
